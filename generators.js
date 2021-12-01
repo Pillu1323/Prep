3a49033535429c31aadef6eeed6c1ff398c70dbf
Pillu1323 <95323937+Pillu1323@users.noreply.github.com>
2021-12-01 10:41:18 +0530

    Add files via upload

--- generators.js
+++ generators.js
@@ -1,6 +1,5 @@
 // generators are the functions that can be paused and resumed as many times we like,
 // each pause it can yield a value back, if resumed it can yield another value back
-// for pause we use yield, for
 
 function* g1() {
   console.log('hello');
@@ -16,16 +15,3 @@ console.log(g.next().value);
 console.log(g.next().value);
 
 // https://www.youtube.com/watch?v=dcP039DYzmE
-
-// Practice
-
-// function* s() {
-//   console.log('Hello');
-//   yield 'Yield 1 ran';
-//   console.log('World');
-//   yield 'Yield 2 ran';
-//   return 'Returned';
-// }
-
-// let s1 = s();
-// console.log(s1.next().done);
